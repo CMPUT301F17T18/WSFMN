47771c0ee2972c51fe5356927dc9ee568e064a1b
Siddhant Khanna <skhanna1@cs.ualberta.ca>
2017-10-16 15:47:27 -0600

    HabitEvent

---
+++ HabitTracker/app/src/main/java/com/a301t18/wsfmn/habittracker/HabitEvent.java
@@ -0,0 +1,15 @@
+package com.a301t18.wsfmn.habittracker;
+
+/**
+ * Created by skhanna1 on 10/16/17.
+ */
+
+public class HabitEvent {
+    String comments;
+    Habit type;
+
+    public class void location(){}
+    public class void AddPic(){}
+
+
+}
