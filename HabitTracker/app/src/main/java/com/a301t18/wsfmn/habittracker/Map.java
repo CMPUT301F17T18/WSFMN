b3eb77697ae6cb3344c198344c51cb0941fa4d19
WeiLi5 <31675721+WeiLi5@users.noreply.github.com>
2017-10-16 15:45:56 -0600

    Map

---
+++ HabitTracker/app/src/main/java/com/a301t18/wsfmn/habittracker/Map.java
@@ -0,0 +1,17 @@
+package com.a301t18.wsfmn.habittracker;
+
+/**
+ * Created by ${WeiLi5} on ${12}.
+ */
+
+public class Map {
+
+    public void highlightEvent(){
+
+    }
+
+    public void friendsEvent(){
+
+    }
+    
+}
