1cbccafb4a5da3cb0ca2819d4199ff60306ea36b
Sidkhanna96 <skhanna1@ualberta.ca>
2017-10-22 13:14:12 -0600

    Changes Made To HabitEvent With TestCases

--- app/src/main/java/com/wsfmn/habittracker/HabitEvent.java
+++ app/src/main/java/com/wsfmn/habittracker/HabitEvent.java
@@ -1,5 +1,10 @@
 package com.wsfmn.habittracker;
 
+import android.media.Image;
+import android.widget.ImageView;
+
+import org.apache.commons.lang3.ObjectUtils;
+
 import java.util.Date;
 
 /**
@@ -11,14 +16,34 @@ public class HabitEvent {
     Habit habit;
     Date date;
 
+    Integer location;
+    //Will change to appropriate Data Type when implement it(ImageView).
+    String pic;
+
     public HabitEvent(Habit habit, Date date, String comment) {
         this.habit = habit;
         this.date = date;
         this.comment = comment;
     }
 
-    public void location(){}
-    public void AddPic(){}
+    public void location(){this.location = 5;}
+    public void AddPic(){this.pic = "Image";}
+    public void updateHabitHistory(){}
+
+    public String getComment(){return comment;}
+
+    public void setComment(String comment){this.comment = comment;}
+
+    public Date getDate(){return date;}
+
+    public void setDate(Date date){this.date = date;}
+
+
+    public Habit getHabit(){return habit;}
+
+    public Integer getLocation(){return this.location;}
+
+    public String getPic(){return this.pic;}
 
 
 }
