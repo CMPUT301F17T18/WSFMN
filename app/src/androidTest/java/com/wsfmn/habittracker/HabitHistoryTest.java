19af4da735c002a143be5aceb515bfdf751e7fd4
nmayne <nmayne@ualberta.ca>
2017-10-21 23:04:45 -0600

    working on HabitHistory

--- app/src/androidTest/java/com/wsfmn/habittracker/HabitHistoryTest.java
+++ app/src/androidTest/java/com/wsfmn/habittracker/HabitHistoryTest.java
@@ -17,13 +17,26 @@ public class HabitHistoryTest extends ActivityInstrumentationTestCase2 {
 
     public void testAddHabitEvent() {
         HabitHistory habitHistory = new HabitHistory();
-        assertNull(habitHistory.getHabitEventAt(0));     // there is no habit event history yet
+        assertNull(habitHistory.getHabitEventAt(0));        // there is no habit event history yet
 
         HabitEvent habitEvent = new HabitEvent(new Date(), "I ate all the pizza!");
-        habitHistory.addHabitEvent(habitEvent);
-        assertNotNull(habitHistory.getHabitEventAt(0));  // there is now a habit event in the history
+        habitHistory.add(habitEvent);
+        assertNotNull(habitHistory.getHabitEventAt(0));     // the habit event is in the history
 
-        assertTrue(habitEvent.getComment().equals("I ate all the pizza!"));
+        HabitEvent receivedHabitEvent = habitHistory.getHabitEventAt(0);
+        assertTrue(habitEvent.getComment().equals(receivedHabitEvent.getComment()));    // same habit event
+
+        assertNull(habitHistory.getHabitEventAt(1));    // there is only one habitEvent in the history
+    }
+
+
+    public void testRemoveHabitEvent() {
+        HabitHistory habitHistory = new HabitHistory();
+        HabitEvent habitEvent = new HabitEvent(new Date(), "I ate all the pizza!");
+        habitHistory.add(habitEvent);
+        assertNotNull(habitHistory.getHabitEventAt(0));     // the habit event is in the history
+
+        habitHistory
     }
 
 }
--- app/src/main/java/com/wsfmn/habittracker/HabitHistory.java
+++ app/src/main/java/com/wsfmn/habittracker/HabitHistory.java
@@ -12,17 +12,21 @@ public class HabitHistory {
     private ArrayList<HabitEvent> habitHistory = new ArrayList<HabitEvent>();
 
 
+
+    public void add(HabitEvent habitEvent) {
+        habitHistory.add(habitEvent);
+    }
+
     public HabitEvent getHabitEventAt(int index) {
         if (habitHistory.isEmpty()) {
             return null;
-        } else if (index > habitHistory.size()){
+        } else if (index >= habitHistory.size()){
             return null;
         } else {
             return habitHistory.get(index);
         }
     }
 
-    public void addHabitEvent(HabitEvent habitEvent) {
-        habitHistory.add(habitEvent);
-    }
+    public int remove
+
 }
