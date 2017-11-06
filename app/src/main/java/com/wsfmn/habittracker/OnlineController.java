5a96814fe73f97d1886830824e8a544fdd505e53
FredricM <fmendi@ualberta.ca>
2017-11-06 09:59:44 -0700

    PROFILE CLASSES

--- app/src/main/java/com/wsfmn/habittracker/OnlineController.java
+++ app/src/main/java/com/wsfmn/habittracker/OnlineController.java
@@ -16,6 +16,7 @@ import io.searchbox.core.Search;
 import io.searchbox.core.SearchResult;
 
 
+
 /**
  * Created by nicholasmayne on 2017-10-16.
  */
---
+++ app/src/main/java/com/wsfmn/habittracker/RequestList.java
@@ -0,0 +1,30 @@
+package com.wsfmn.habittracker;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Fredric on 2017-11-03.
+ */
+
+public class RequestList {
+    private ArrayList<Request> requests = new ArrayList<Request>();
+
+    public void addTweet(Request request){
+        requests.add(request);
+    }
+
+
+    public void deleteTweet(Request request){
+        requests.remove(request);
+
+    }
+
+    public boolean hasTweet(Request request){
+        return requests.contains(request);
+    }
+
+    public Request getTweet(int index){
+        return requests.get(index);
+    }
+
+}
