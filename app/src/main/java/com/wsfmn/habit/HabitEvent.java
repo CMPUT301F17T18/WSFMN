4a3798661d46e77bb8f38e8033c9a257c1b47c90
nmayne <nmayne@ualberta.ca>
2017-11-10 14:05:58 -0700

    fixing merge errors

--- app/src/main/java/com/wsfmn/habit/HabitEvent.java
+++ app/src/main/java/com/wsfmn/habit/HabitEvent.java
@@ -78,6 +78,10 @@ public class HabitEvent{
 
     public Bitmap getImage(){return image;}
 
+    public void setImage(Bitmap bit) {
+        this.image = bit;
+    }
+
 //    public void setLocation(Geolocation location){
 //        this.location = location;
 //    }
