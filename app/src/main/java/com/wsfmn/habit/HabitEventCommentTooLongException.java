32679aa0bf18df568eecb4ae093c5369c0c82c98
Sidkhanna96 <skhanna1@ualberta.ca>
2017-11-12 10:48:40 -0700

    Final

--- app/src/main/java/com/wsfmn/habit/HabitEventCommentTooLongException.java
+++ app/src/main/java/com/wsfmn/habit/HabitEventCommentTooLongException.java
@@ -4,6 +4,8 @@ package com.wsfmn.habit;
  * Created by siddhant on 2017-11-11.
  */
 
+/*Exception for Comments which is thrown when the user exceeds
+* the 20 character limit for the Habit Event*/
 public class HabitEventCommentTooLongException extends Exception {
 
     public HabitEventCommentTooLongException(){
