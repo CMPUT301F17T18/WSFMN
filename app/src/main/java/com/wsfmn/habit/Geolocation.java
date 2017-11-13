ada73b6a9a1b625d82e076b7304e0613eff845e2
WeiLi5 <31675721+WeiLi5@users.noreply.github.com>
2017-11-12 22:11:00 -0700

    update

--- app/src/main/java/com/wsfmn/habit/Geolocation.java
+++ app/src/main/java/com/wsfmn/habit/Geolocation.java
@@ -12,32 +12,58 @@ public class Geolocation {
     private Address myAddress;
     private LatLng latLng;
 
+    /**
+     *  Created a Habit object
+     *
+
+     */
+
     public Geolocation(){
         myAddress = null;
         latLng = null;
 
     }
-
+    /**
+     *  Created a Habit object
+     *
+     * @param myAddress is the address that can be identified by google map
+     * @param latLng is a coordination(latitude, longtitude)
+     */
     public Geolocation(Address myAddress, LatLng latLng){
         this.setMyAddress(myAddress);
         this.setLatLng(latLng);
 
     }
-
+    /**
+     * @Return return the address
+     */
     public Address getAddress(){
         return this.myAddress;
 
     }
-
+    /**
+     * @Return return the latLng
+     *
+     */
     public LatLng getLatLng(){
         return this.latLng;
 
     }
 
+    /**
+     *
+     * @param myAddress the address of a location
+     */
+
     public void setMyAddress(Address myAddress){
         this.myAddress = myAddress;
 
     }
+
+    /**
+     *
+     * @param latLng the coordination of a location
+     */
 
     public void setLatLng(LatLng latLng){
         this.latLng = latLng;
